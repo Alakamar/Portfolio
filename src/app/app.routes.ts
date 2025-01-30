f4503295f36ccc5f32ab5e1c3dc85cc9b4cde44b
Adrien Hiribarren Touya <adrien64250@yahoo.com>
2025-01-30 16:39:28 +0100

    ✨ Initialisation du projet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { AccueilComponent } from "../components/accueil/accueil.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: "",
+		redirectTo: "accueil",
+		pathMatch: "full",
+	},
+	{
+		path: "accueil",
+		component: AccueilComponent,
+	},
+	{
+		path: "**",
+		redirectTo: "accueil",
+	},
+];
---
+++ src/components/accueil/accueil.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "app-accueil",
+	imports: [],
+	templateUrl: "./accueil.component.html",
+	styleUrl: "./accueil.component.scss",
+})
+export class AccueilComponent {}
